1b73440b94a0bac002259ad7e7c2fceb43ca32b0
MariaPittari <56306948+MariaPittari@users.noreply.github.com>
2020-02-16 15:34:25 +0100

    Update controller_node.py

--- controller_node.py
+++ controller_node.py
@@ -6,11 +6,15 @@ import numpy as np
 from geometry_msgs.msg import Twist
 from std_msgs.msg import IntString
 
-
-class DecisionNode(object):
+# nodo controllore che fa da subscriber per tre topic su cui pubblicano il nodo relativo agli ostacoli, quello che permette di seguire la linea e il
+# nodo di pianificazione. E' publisher per il topic "cmd_vel_topic" a cui passa i valori di velocità lineare e angolare secondo
+# dei criteri di priorità.
+class ControllerNode(object):
 
     def __init__(self):
-        self.orientation = None
+        self.orientation = None # serve per gestire le situazioni in cui il robot gira a dx (o a sx) per un ostacolo e quando
+                                # vede nuovamente la linea sempre a dx (o a sx) continua a girare troppo andano a sbattere contro
+                                # la parete. Quindi in questa situazione lo facciamo girare in direzione opposta
         self.curve = False
         self.qr_distance = 200
         self.ob_linear = -1000
@@ -23,54 +27,60 @@ class DecisionNode(object):
         rospy.Subscriber("change_obstacle", Twist, self.decision_callback, 1)
         rospy.Subscriber("planning_topic", IntString, self.decision_callback, 2)
         self.pub = rospy.Publisher("cmd_vel_topic", Twist, queue_size=1)
-
+    # callback invocata ogni volta che qualcosa è pubblicato su uno dei nodi per cui è subscriber
     def decision_callback(self, data, args):
-        rotation = 0
-        speed = Twist()
-        if args == 0:
-            self.line_linear = data.linear.x
+        rotation = 0 # inizializzata a 0 ogni volta che si chiama una callback, serve per identificare configurazioni non permesse al robot
+        speed = Twist() # creazione del messaggio di tipo Twist da pubblicare
+        if args == 0: # callback relativa al "line_foll_topic" aggiorna i valori di velocità lineare e angolare della linea
+            self.line_linear = data.linear.x 
             self.line_angular = data.angular.z
-        elif args == 2:
+        elif args == 2: # callback relativa al "planning_topic", aggiorna il valore di rotation, distanza dal landmark e dice se il landmark visto è prossimo a una curva
             rotation = data.rotation
             self.qr_distance = data.distance
             self.curve = data.curve
-        else:
+        else: # callback relativa ad "change_obstacle",  imposta i valori delle velocità angolare e lineare degli ostacoli 
             self.ob_linear = data.linear.x
             self.ob_angular = data.angular.z
 
         # se non ci sono ostacoli da considerare
         if (self.ob_linear == -1000) and (self.ob_angular == -1000):
-            # se
+            # se il robot era già andato a sinistra e velocità angolare della linea suggerisce di andare di nuovo a destra
+            # oppure se il robot erà già andato a destra e la velocità angolare della linea suggerisce di andare di nuovo a 
+            # sinistra -> lo facciamo girare in verso opposto per evitare che girando troppo in una direzione vada a sbattere
+            # contro la parete
             if (self.orientation == 0 and self.line_angular < 0) or (self.orientation == 1 and self.line_angular > 0):
                 self.angular = self.line_angular * -1
-            else:
+            else: # se non siamo nella situazione critica precedente passa l'accelerazione angolare così come pubblicata
+                  # dal nodo line_follower
                 self.angular = self.line_angular
             self.orientation = None
             self.linear = self.line_linear
             print("{DECISION_NODE} SPEED==> Line: [" + str(self.linear) + "," + str(self.angular) + "]")
         # se invece ci sono gli ostacoli da considerare
         else:
-            if self.ob_angular < 0:  # sinistra
+            if self.ob_angular < 0:  # accelerazione angolare negativa, il robot gira a sx
                 self.orientation = 0
-            else:  # destra
+            else:  # accelerazione angolare positiva il robot gira a dx
                 self.orientation = 1
             self.linear = self.ob_linear
             self.angular = self.ob_angular
             print("{DECISION_NODE} SPEED==> Obstacle: [" + str(self.linear) + "," + str(self.angular) + "]")
 
-        if rotation == 1:
+        if rotation == 1: # il robot deve tornare indietro perchè sta assumendo una configurazione non ammessa
             if (self.curve and self.qr_distance > 30) or not self.curve:
+                # se l'ultimo landmark visto era uno di curva e la distanza da esso era > 30 oppure se non era di curva , significa
+                # che il robot sta andando contro le pareti
                 print("TURN OPPOSITE")
-                self.linear = self.linear * -1
-                self.qr_distance = 0
-            else:
+                self.linear = self.linear * -1 # il robot fa un passo indietro
+                self.qr_distance = 0 
+            else: # il robot sta facendo la curva 
                 print("Doing curve!")
-        elif rotation == 2:
+        elif rotation == 2: #il robot è in una configurazione non ammessa , quindi ruota di 180°
             self.linear = 0
             self.angular = self.angular * 2
         else:
             print("Doing great!")
-
+        
         speed.linear.x = self.linear
         speed.angular.z = self.angular
 
@@ -80,5 +90,5 @@ class DecisionNode(object):
 
 if __name__ == "__main__":
     rospy.init_node("decision_node", anonymous=True)
-    ob = DecisionNode()
+    ob = Controller()
     rospy.spin()
