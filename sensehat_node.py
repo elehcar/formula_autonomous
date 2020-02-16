22c224c7dea0faf5dea52f21b7c7ffcb4a36535a
MariaPittari <56306948+MariaPittari@users.noreply.github.com>
2020-02-16 14:42:55 +0100

    Update sensehat_node.py

--- sensehat_node.py
+++ sensehat_node.py
@@ -3,13 +3,13 @@ import rospy
 from std_msgs.msg import Float32
 from sense_hat import SenseHat
 
-
+# nodo che pubblica sul topic "magnetometer_topic" un valore float che rappresenta la rotazione rispetto al Nord geomagnetico
 class SenseNode:
 	def __init__(self):
 		self.node_rate = 10
 		self.sense = SenseHat()
 		self.pub = rospy.Publisher('magnetometer_topic', Float32, queue_size=10)
-
+	# funzione che usa il metodo get_compass della classe SenseHat che restituisce i gradi rispetto al Nord magnetico
 	def sensing(self):
 		north = self.sense.get_compass()
 		self.pub.publish(north)
