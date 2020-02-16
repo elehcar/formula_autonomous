1b8998906e7654ac10f0a3859ca309b5a0b86639
MariaPittari <56306948+MariaPittari@users.noreply.github.com>
2020-02-16 14:39:58 +0100

    Update line_follower_node.py

--- line_follower_node.py
+++ line_follower_node.py
@@ -6,20 +6,26 @@ import numpy as np
 from std_msgs.msg import TwoBool, Int16
 from geometry_msgs.msg import Twist
 
-
+# nodo che prendendo i valori restituiti dagli infrarossi attraverso il topic "ir_node_topic" cambia i valori di velocità angolare e lineare
+# per permettere al robot di seguire la linea.
 class LineFollower(object):
 
     def __init__(self, linear_vel_base, angular_vel_base):
         self.counter = 0
         self.ir_sub = rospy.Subscriber("ir_node_topic", TwoBool, self.line_foll_callback)
+	# topic per utilizzare i giri non usato?????
         self.line_planning_pub = rospy.Publisher("count_round", Int16, queue_size=1)
         self.line_foll_pub = rospy.Publisher("line_foll_topic", Twist, queue_size=1)
+	# creazione del messaggio che sarà pubblicato su "line_foll_topic", i cui valori di velocità lineare e angolare sono
+	# inizializzati entrambi a 0
         self.cmd_vel = Twist()
         self.cmd_vel.linear.x = 0.0
         self.cmd_vel.angular.z = 0.0
+	# valori di base della velocità lineare e angolare passati come parametri al momento della creazione di un oggetto della classe
         self.linear_vel_base = linear_vel_base
         self.angular_vel_base = angular_vel_base
-
+    # callback invocata quando viene pubblicato qualcosa sul topic "ir_node_topic" e che pubblica sul topic "line_foll_topic"
+    # i valori relativi alla velocità angolare e lineare che permettono di seguire la linea.
     def line_foll_callback(self, ir_sensors):
         left_ir = ir_sensors.left_ir
         right_ir = ir_sensors.right_ir
@@ -32,8 +38,8 @@ class LineFollower(object):
             self.cmd_vel.linear.x = -1000
         elif left_ir and not right_ir:
             # il sensore sx rileva la linea -> vado a sinistra
-            self.cmd_vel.angular.z = self.angular_vel_base * -1
-            self.cmd_vel.linear.x = self.linear_vel_base
+            self.cmd_vel.angular.z = self.angular_vel_base * -1 # rendo la velocità angolare di base negativa
+            self.cmd_vel.linear.x = self.linear_vel_base # la velocità lineare è quella di base
             print("{LINE_FOLLOWER} SPEED==>[" + str(self.cmd_vel.linear.x) + "," + str(self.cmd_vel.angular.z) + "]")
 
         elif right_ir and not left_ir:
@@ -43,7 +49,7 @@ class LineFollower(object):
             print("{LINE_FOLLOWER} SPEED==>[" + str(self.cmd_vel.linear.x) + "," + str(self.cmd_vel.angular.z) + "]")
 
         else:
-            # sono nella carreggiata -> pubblico non fare nulla
+            # sono nella carreggiata -> pubblico i valori di default
             self.cmd_vel.angular.z = -1000
             self.cmd_vel.linear.x = -1000
 
