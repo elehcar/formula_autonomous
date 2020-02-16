fa8690d92a68734d7200f9e0484b76acd657d1e3
MariaPittari <56306948+MariaPittari@users.noreply.github.com>
2020-02-16 14:37:36 +0100

    Update infrared_node.py

--- infrared_node.py
+++ infrared_node.py
@@ -7,21 +7,26 @@ import time
 from std_msgs.msg import TwoBool
 IO.setwarnings(False)
 
-
+# nodo che pubblica sul topic "ir_node_topic" i due valori booleani relativi all'infrarosso destro e all'infrarosso sinistro
 class IrNode(object):
 
     def __init__(self):
         self.node_rate = 10
         IO.setmode(IO.BCM)
         self.line_foll_pub = rospy.Publisher("ir_node_topic", TwoBool, queue_size=1)
+        # settiamo le GPIO 10 e 9 in pin di input
         IO.setup(10, IO.IN)  # LEFT IR
         IO.setup(9, IO.IN)  # RIGHT IR
 
     def ir_function(self):
+	#creazione del messaggio di tipo TwoBool e assegnazione alle sue componenti left e right
+	#di di ciò che leggono rispettivamente la GPIO 10 e la GPIO 9. Se gli infrarossi rilevano la linea nera restituiscono 
+	#true altrimenti false. 
         ir_sensors = TwoBool()
         ir_sensors.left_ir = IO.input(10)
         ir_sensors.right_ir = IO.input(9)
         print("{INFRAROSSI}: sensing!")
+	# pubblicazione del messaggio su topic
         self.line_foll_pub.publish(ir_sensors)
 
 
@@ -31,4 +36,4 @@ if __name__ == "__main__":
     loop = rospy.Rate(ir_node.node_rate)
     while not rospy.is_shutdown():
         ir_node.ir_function()
-        loop.sleep()
+	    loop.sleep()
